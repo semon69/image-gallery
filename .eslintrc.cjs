7e3d3deb714eba2922062b8d07bd1eabb197a418
semon69 <smdemon3@gmail.com>
2023-10-31 14:49:40 +0600

    setup react

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+  "eslint:recommended",
+  "plugin:react/recommended",
+  "plugin:react/jsx-runtime",
+  "plugin:react-hooks/recommended",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+  "react-refresh/only-export-components": "warn",
+  "no-unused-vars": 0,
+  "no-undef": 0,
+  "react/prop-types": 0,
+  "react/no-unescaped-entities": 0
+  },
+  };
